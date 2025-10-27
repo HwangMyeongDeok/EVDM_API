365e2015e834dcf0ff578ef4b265cbcdb707d4a6
minhduc1706 <minhduc0879395@gmail.com>
2025-10-27 08:05:06 +0700

    add route

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,20 +1,27 @@
 import { Router } from 'express';
-import vehicleRoute from './vehicles/vehicle.route'; 
+import vehicleRoute from './vehicles/vehicle.route';
 import quotationRoute from './quotation/quotation.route';
+import customerRoute from './customer/customer.route';
+import contractRoute from './contract/contract.route';
+import paymentRoute from './payment/payment.route';
+
 const router = Router();
 
 interface IRoute {
-    path: string;
-    route: Router;
+  path: string;
+  route: Router;
 }
 
 const appRoutes: IRoute[] = [
-    { path: '/vehicles', route: vehicleRoute },
-    { path: '/quotations', route: quotationRoute },
+  { path: '/vehicles', route: vehicleRoute },
+  { path: '/quotations', route: quotationRoute },
+  { path: '/customers', route: customerRoute },
+  { path: '/contracts', route: contractRoute },
+  { path: '/payments', route: paymentRoute },
 ];
 
 appRoutes.forEach((route) => {
-    router.use(route.path, route.route);
+  router.use(route.path, route.route);
 });
 
 export default router;
